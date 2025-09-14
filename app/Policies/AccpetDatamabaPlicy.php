84d2af8ef99bb537f9c555ec5ef75cdbd6f0971c
Vincent1920 <vincentluhulima600@gmail.com>
2025-09-14 11:41:37 +0700

    JadawalKegiatan

--- app/Policies/AccpetDatamabaPlicy.php
+++ app/Policies/AccpetDatamabaPlicy.php
@@ -5,25 +5,53 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\AccpetDatamaba;
 
-class AccpetDatamabaPlicy
+class AccpetDatamabaPolicy
 {
     /**
      * Create a new policy instance.
      */
     public function __construct()
     {
-        
-    }
-    public function viewAny(User $user): bool
-    {
-        return $user->email === 'admin@gmail.com';
+        //
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view any data maba.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can view a specific data maba.
      */
     public function view(User $user, AccpetDatamaba $dataMahasiswa): bool
     {
-        return $user->email === 'admin@gmail.com';
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can create new data maba.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can update the data maba.
+     */
+    public function update(User $user, AccpetDatamaba $dataMahasiswa): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can delete the data maba.
+     */
+    public function delete(User $user, AccpetDatamaba $dataMahasiswa): bool
+    {
+        return $user->role === 'admin';
     }
 }
